d8af10f381a78be3810573c91218ed4c4bd83eb5
Metehan Beyaz <metehanbyz@outlook.com>
2022-05-01 17:07:37 +0000

    Get sparked!

--- prop.mk
+++ prop.mk
@@ -10,3 +10,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0
 
+# Spark Props
+ro.spark.maintainer=Metehan Beyaz
+ro.spark.device.name=Mi A1
+ro.spark.group.url=https://t.me/MetoIsTaken
+ro.spark.maintainer.username=MetoIsTaken
+
