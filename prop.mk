043c107babc6d3a0d1aee30bec2a49cc65cda95a
Metehan Beyaz <metehanbyz@outlook.com>
2022-05-08 14:08:17 +0000

    Update maintainer strings

--- prop.mk
+++ prop.mk
@@ -10,9 +10,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0
 
-# Spark Props
-ro.spark.maintainer=Metehan Beyaz
-ro.spark.device.name=Mi A1
-ro.spark.group.url=https://t.me/MetoIsTaken
-ro.spark.maintainer.username=MetoIsTaken
-
