ec567a9e4f567850b8089ad510fc02822587359a
Metehan Beyaz <metehanbyz@outlook.com>
2022-05-10 17:28:54 +0000

    props: Enable blurs
    * Disabled by default
    * Also enable expensive rendering for blurs
    * Disabled blur on app launch

--- prop.mk
+++ prop.mk
@@ -10,3 +10,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0
 
+# Blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.surface_flinger.supports_background_blur=1 \
+ro.sf.blurs_are_expensive=1 \
+persist.sys.sf.disable_blurs=1 \
+ro.launcher.blur.appLaunch=0
